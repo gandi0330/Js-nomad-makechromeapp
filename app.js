8c7de1a75d4cd0dc27b7905fc478616948bb3abc
hm <gandi0330@naver.com>
2022-03-09 23:35:18 +0900

    feat(login) : username을 localStorage에 저장하고 로드하는 기능 추가

--- app.js
+++ app.js
@@ -4,14 +4,30 @@ const loginButton = loginForm.querySelector("button");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(e){
     e.preventDefault();
+
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+
     loginForm.classList.add(HIDDEN_CLASSNAME);
+
+    paintGreetings(username);
+}
+
+function paintGreetings(username){
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 
-loginForm.addEventListener("submit",onLoginSubmit);
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit",onLoginSubmit);
+} else {
+    paintGreetings(savedUsername);
+}
