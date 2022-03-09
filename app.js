13017b249c4ada7dd9c04a599945863a5c9c13de
hm <gandi0330@naver.com>
2022-03-09 23:12:28 +0900

    feat(login) : username 과 login 버튼 생성 및 유효성 검사 기능 추가

---
+++ app.js
@@ -0,0 +1,17 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(e){
+    e.preventDefault();
+    const username = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+
+loginForm.addEventListener("submit",onLoginSubmit);
